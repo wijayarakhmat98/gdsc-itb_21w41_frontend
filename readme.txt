5e5f276e2bce237d8fa87bc290b079983bc5b407
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2021-10-18 19:31:20 +0700

    Data dump in JSON file

---
+++ readme.txt
@@ -0,0 +1,7 @@
+from    https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server
+
+use     python3 -m http.server
+
+to host a local web server, then access it through
+
+        localhost:8000
--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-let wiggle_box = document.getElementById("wiggle_box");
-let wiggle = document.getElementById("wiggle");
+(() => {
+	let wiggle_box = document.getElementById("wiggle_box");
+	let wiggle = document.getElementById("wiggle");
 
-const s = 2;
-let t = 0;
-let d = true;
+	wiggle.style.display = "inline-block";
 
-wiggle.style.display = "inline-block";
+	const s = 2;
+	let t = 0;
+	let d = true;
 
-setInterval(()=>{
-	if (d) {
-		t += s;
-		if (t >= wiggle_box.clientWidth-wiggle.offsetWidth) {
-			d = false;
+	setInterval(()=>{
+		if (d) {
+			t += s;
+			if (t >= wiggle_box.clientWidth-wiggle.offsetWidth) {
+				d = false;
+			}
+		} else {
+			t -= s;
+			if (t <= 0) {
+				d = true;
+			}
 		}
-	} else {
-		t -= s;
-		if (t <= 0) {
-			d = true;
-		}
-	}
+		wiggle.style.transform = "translateX(" + t + "px)";
+	}, 16.67);
+})();
 
-	wiggle.style.transform = "translateX(" + t + "px)";
-}, 16.67);
+async function get_json(file) {
+	const response = await fetch(file);
+	const json = response.json();
+	return json;
+}
+
+get_json("data.json").then((data) => {
+	const body = document.getElementsByTagName("body")[0];
+	data.forEach((i) => {
+		let heading = document.createElement("h1");
+		heading.appendChild(document.createTextNode(i.title));
+
+		let description = document.createElement("p");
+		description.appendChild(document.createTextNode(i.description));
+
+		let block = document.createElement("div");
+		block.appendChild(heading);
+		block.appendChild(description);
+
+		body.appendChild(block);
+	});
+});
