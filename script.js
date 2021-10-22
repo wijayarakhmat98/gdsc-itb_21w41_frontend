cbce03a421fb5fa7aa8317b54242f1373e49e50a
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2021-10-22 19:56:53 +0700

    Create catalog template

--- script.js
+++ script.js
@@ -1,48 +1,30 @@
-(() => {
-	let wiggle_box = document.getElementById("wiggle_box");
-	let wiggle = document.getElementById("wiggle");
-
-	wiggle.style.display = "inline-block";
-
-	const s = 2;
-	let t = 0;
-	let d = true;
-
-	setInterval(()=>{
-		if (d) {
-			t += s;
-			if (t >= wiggle_box.clientWidth-wiggle.offsetWidth) {
-				d = false;
-			}
-		} else {
-			t -= s;
-			if (t <= 0) {
-				d = true;
-			}
-		}
-		wiggle.style.transform = "translateX(" + t + "px)";
-	}, 16.67);
-})();
-
 async function get_json(file) {
-	const response = await fetch(file);
-	const json = response.json();
-	return json;
+	const response = await fetch(file)
+	const json = response.json()
+	return json
 }
 
-get_json("data.json").then((data) => {
-	const body = document.getElementsByTagName("body")[0];
-	data.forEach((i) => {
-		let heading = document.createElement("h1");
-		heading.appendChild(document.createTextNode(i.title));
+const container = document.getElementById("container")
+const template = document.getElementById("template")
+container.removeChild(template)
+template.removeAttribute("id")
 
-		let description = document.createElement("p");
-		description.appendChild(document.createTextNode(i.description));
+get_json("data.json").then((
+	data
+) => {
+	data.forEach((
+		movie
+	) => {
+		let catalog = template.cloneNode(true)
 
-		let block = document.createElement("div");
-		block.appendChild(heading);
-		block.appendChild(description);
+		let cover = catalog.getElementsByTagName("img")[0]
+		let name = catalog.getElementsByTagName("p")[0]
+		let ratings = catalog.getElementsByTagName("p")[1]
 
-		body.appendChild(block);
-	});
-});
+		cover.setAttribute("src", movie.cover)
+		cover.innderHTML = movie.alternative
+		name.innerHTML = movie.title
+
+		container.appendChild(catalog)
+	})
+})
