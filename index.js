693688be92f3a036af6d860fbe75b2a3b05dc6f9
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2021-10-28 15:51:18 +0700

    Implement search function

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-async function get_json(file) {
-	const response = await fetch(file)
-	const json = response.json()
-	return json
+let context = []
+
+function set_data(i) {
+	sessionStorage.setItem('data', JSON.stringify(context[i]))
 }
 
-const json = get_json("data.json")
+function set_new_data(i) {
+	localStorage.setItem('data', JSON.stringify(context[i]))
+}
 
 const container = document.getElementById("container")
 const template = document.getElementById("template")
-
-container.removeChild(template)
 template.removeAttribute("id")
 
-json.then((data) => {
-	for (let i = 0; i < data.length; ++i) {
-		let movie = data[i]
+function render() {
+	container.innerHTML = ""
+
+	for (let i = 0; i < context.length; ++i) {
+		let movie = context[i]
 		let catalog = template.cloneNode(true)
 
 		let cover = catalog.getElementsByClassName("cover")[0]
@@ -36,18 +38,43 @@ json.then((data) => {
 
 		container.appendChild(catalog)
 	}
+}
 
+async function get_json(file) {
+	const response = await fetch(file)
+	const json = response.json()
+	return json
+}
+
+const json = get_json("data.json")
+
+json.then((data) => {
+	context = data
+	render()
 	container.removeAttribute("style")
 })
 
-function set_data(i) {
+const field = document.getElementById("search")
+field.addEventListener('change', search)
+
+function search() {
 	json.then((data) => {
-		sessionStorage.setItem('data', JSON.stringify(data[i]))
+		const text = field.value.toLowerCase()
+		context = []
+		data.forEach((movie) => {
+			const title = movie.title.toLowerCase()
+			if (title.search(text) != -1) {
+				context.push(movie)
+			}
+		})
+		render()
 	})
 }
 
-function set_new_data(i) {
+function reset() {
 	json.then((data) => {
-		localStorage.setItem('data', JSON.stringify(data[i]))
+		field.value = ""
+		context = data
+		render()
 	})
 }
